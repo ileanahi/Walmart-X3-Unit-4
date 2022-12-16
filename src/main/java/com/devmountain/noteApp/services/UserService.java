f1f20b7ea79d66e8f2bc335e9d62c7b68436d05d
ileanahi <argyris.ileana@gmail.com>
2022-12-15 23:57:51 -1000

    Initial commit

---
+++ src/main/java/com/devmountain/noteApp/services/UserService.java
@@ -0,0 +1,14 @@
+package com.devmountain.noteApp.services;
+
+import com.devmountain.noteApp.UserDto;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+public interface UserService {
+    @Transactional
+    List<String> addUser(UserDto userDto);
+
+    @Transactional
+    List<String> userLogin(UserDto userDto);
+}
